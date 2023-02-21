0a021eb66e35bd98be8c06ed688bff64b3a1a35b
Sven Killig <sven@killig.de>
2023-02-21 04:13:38 +0100

    added form link

--- App/BalaC-Plus/Arduino/BalaCplus/OTAWebUpdater.h
+++ App/BalaC-Plus/Arduino/BalaCplus/OTAWebUpdater.h
@@ -21,6 +21,7 @@ WebServer server(80);
 
 const char* serverIndex = 
 "<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>"
+"<a href='/form'>form</a><p>"
 "<form method='POST' action='#' enctype='multipart/form-data' id='upload_form'>"
    "<input type='file' name='update'>"
         "<input type='submit' value='Update'>"
